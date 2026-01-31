e92602d24dfedd384df4322f80d940835c1f07bc
Morphes <92577219+Morphess@users.noreply.github.com>
2026-01-31 12:49:55 +0200

    Add data and customer creation logic

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,15 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getData() {
+        return new String[]{"Maks", "555 123-8596"};
+    }
+
+    public static Customer getCustomer(String [] data) {
+        return new Customer(data[0], data[1]);
+    }
 }
