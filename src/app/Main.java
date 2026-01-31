ab0c5d86a636c0e4ae45844caec743f5f6f50e7b
Morphes <92577219+Morphess@users.noreply.github.com>
2026-01-31 13:05:59 +0200

    Add customer info output

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,11 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName()
+                + ", phone " + customer.getPhone();
+
+        getOutput(output);
 
     }
 
@@ -11,5 +16,9 @@ public class Main {
 
     public static Customer getCustomer(String [] data) {
         return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
     }
 }
