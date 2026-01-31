204b925a5c2cc3630ee250dc5e377b72e9389cb7
Morphes <92577219+Morphess@users.noreply.github.com>
2026-01-31 12:43:08 +0200

    Add constructor to Customer

--- src/app/Customer.java
+++ src/app/Customer.java
@@ -1,4 +1,13 @@
 package app;
 
 public class Customer {
+
+    private final String name;
+    private final String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
 }
